b54c6c9a01d41665c0b92baa61743c561cffc6e8
KishanthSelvaraj <154578200+KishanthSelvaraj@users.noreply.github.com>
2024-07-04 19:41:47 +0530

    Update CardProvider.jsx

--- components/CardProvider.jsx
+++ components/CardProvider.jsx
@@ -6,8 +6,8 @@ const CardProvider = ({ children }) => {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://ecommerce-dusky-alpha.vercel.app/success"
+      cancelUrl="https://ecommerce-dusky-alpha.vercel.app/error"
       language="en-US"
       currency="USD"
       billingAddressCollection={true}
